7f71dd3d1bb201b9c33e2b1caa99422a4cb01ba9
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-08-19 23:37:07 +0300

    Update exchangeApi.py

--- ExchangeApi/exchangeApi.py
+++ ExchangeApi/exchangeApi.py
@@ -1,1 +1,14 @@
 
+import requests
+
+base_url = 'https://api.binance.com'
+
+path = '/api/v3/exchangeInfo'
+
+r = requests.get(base_url+path)
+
+params = '?symbol=BTCUSDT&interval=1m'
+
+r = requests.get(base_url+path,params={'symbol':'BTCUSDT','interval':'1m'})
+
+print(r.json)
