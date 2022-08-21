50143d6cf3569774da949c2754d0250df5037603
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-08-21 14:27:07 +0300

    Update GitHubApi.py

--- GitHubApi/GitHubApi.py
+++ GitHubApi/GitHubApi.py
@@ -6,26 +6,40 @@ class Github:
 
     def __init__(self):
         self.api_url = 'https://api.github.com'
-
+        self.token = 'YOUR ACCESS TOKEN'
 
     def getUser(self,username):
         response = requests.get(self.api_url+'/users/'+username) 
         return response.json()
 
     def getRepository(self,username):
-        response = requests.get(self.api_url+'/users/'+username+'repos')
+        response = requests.get(self.api_url+'/users/'+username+'/repos')
         return response.json()
+
+
+    def createRepository(self,name):
+        response = requests.post(self.api_url+'/user/repos?access_token'+ self.token, json={
+            "name" : name,
+            "description" : "This is your first repository",
+            "homepage" : "https://YOUR ADDRESS.com",
+            "private" : False,
+            "has_issues": True,
+            "has_projects": True,
+            "has_wiki" : True
+        })
+        return response.json()
+        
 
 github = Github()
 
 
 while True:
     choose = input("""
-1- Find User
-2- Get Repositories
-3- Create Repository
-4- Exit
-Your Choose: 
+                    1- Find User
+                    2- Get Repositories
+                    3- Create Repository
+                    4- Exit
+                    Your Choose: 
                     """)    
 
     if choose == '4':
@@ -35,17 +49,22 @@ Your Choose:
             username = input('username: ')
             result = github.getUser(username)
             print(f"""
-Name: {result['name']}
-Public repos: {result['public_repos']}
-Followers: {result['followers']}""")
+                    Name: {result['name']}
+                    Public repos: {result['public_repos']}
+                    Followers: {result['followers']}
+                    """)
 
         elif choose == '2':
             username = input('username: ')
             result = github.getRepository(username)
+            i = 1
             for repo in result:
-                print(repo['name'])
+                print(f"{i} - {repo['name']}")
+                i += 1
 
         elif choose == '3':
-            pass
+            name = input('repository name: ')
+            result = github.createRepository(name)
+            print(result)
         else:
             print("Something went wrong")
