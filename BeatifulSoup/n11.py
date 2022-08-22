2415b63a36dda50d884cf4716fac94365d5cbf9e
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-08-22 13:58:34 +0300

    Create n11.py

---
+++ BeatifulSoup/n11.py
@@ -0,0 +1,23 @@
+
+from cgitb import text
+from hashlib import new
+from urllib import response
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.n11.com/bilgisayar/dizustu-bilgisayar"
+
+html = requests.get(url).content
+
+soup = BeautifulSoup(html, "html.parser")
+
+list = soup.find_all("li", {"class":"column"},limit =50)
+
+for li in list:
+    name = li.div.a.h3.text.strip()
+    link = li.div.a.get("href")
+    oldprice = li.find("div",{"class":"proDetail"}).find("del").text
+    newprice = li.find("div",{"class":"proDetail"}).find("ins").text
+    print(f"Product Name: {name}\nProduct Link: {link}\nOld Price: {oldprice}\nNew Price: {newprice}\n-----------------------------")
+
+# print(list)
