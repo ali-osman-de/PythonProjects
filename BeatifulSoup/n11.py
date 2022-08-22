e0046c0185ad1d5e37ed0550fd17bd8a149730e2
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-08-22 14:01:32 +0300

    Update n11.py

--- BeatifulSoup/n11.py
+++ BeatifulSoup/n11.py
@@ -10,11 +10,13 @@ soup = BeautifulSoup(html, "html.parser")
 
 list = soup.find_all("li", {"class":"column"},limit =50)
 
+count = 1
+
 for li in list:
     name = li.div.a.h3.text.strip()
     link = li.div.a.get("href")
     oldprice = li.find("div",{"class":"proDetail"}).find("del").text
     newprice = li.find("div",{"class":"proDetail"}).find("ins").text
-    print(f"Product Name: {name}\nProduct Link: {link}\nOld Price: {oldprice}\nNew Price: {newprice}\n-----------------------------")
-
+    print(f"{count} - Product Name: {name}\nProduct Link: {link}\nOld Price: {oldprice}\nNew Price: {newprice}\n-----------------------------")
+    count +=1
 # print(list)
